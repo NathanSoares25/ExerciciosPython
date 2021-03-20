99d1a7a2a599df016c8ccbad3d3bd78c2476b2b0
Nathan Soares <nathanbernardino09@gmail.com>
2021-03-20 19:54:49 -0300

    Numero Por Extenso.py
    Programa que ler os números de 1 a 20 por extenso

--- ExerciciosPython/Número por Extenso.py
+++ ExerciciosPython/Número por Extenso.py
@@ -1,8 +1,9 @@
 # Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
            'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-escolha = -1
 while True:
     escolha = int(input('Escolha um número entre 0 e 20'))
-    if 0 < escolha < 20:
-        print(f'')
+    if 0 <= escolha <= 20:
+        break
+    print('Tente novamente.', end='')
+print('Você digitou o número {extenso[escolha]}')  
