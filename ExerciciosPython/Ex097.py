ca598a442d58a4597b29fb8194e48d338ad7d974
Nathan Soares <nathanbernardino09@gmail.com>
2021-05-18 19:59:25 -0300

    Create Ex097.py

---
+++ ExerciciosPython/Ex097.py
@@ -0,0 +1,11 @@
+# Faça um programa que tenha uma função chamada escreva() que receba um texto qualquer como parametro e mostre uma
+# Mensagem com tamanha adaptavel.
+
+def escreva(txt):
+    print('-' * len(txt))
+    print(txt)
+    print('-' * len(txt))
+
+
+escreva('  Nathan Soares  ')
+escreva('  Isabela Gonçalves  ')
